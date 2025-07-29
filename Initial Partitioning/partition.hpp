92fca6110c7530b5b5753f1a57d053ab8f5b0195
qkrdudwls <jacob717@ajou.ac.kr>
2025-07-29 17:48:03 +0900

    update header file

--- Initial Partitioning/partition.hpp
+++ Initial Partitioning/partition.hpp
@@ -1,5 +1,10 @@
+#ifndef PARTITION_HPP
+#define PARTITION_HPP
+
 #pragma once
 #include <vector>
 #include <unordered_map>
 
 void partition_expansion(int procId, int nprocs, int numParts, int theta, const std::vector<int> &seeds, const std::unordered_map<int, int> &global_degree, const std::unordered_map<int, std::vector<int>> &local_adj, std::vector<std::vector<int>> &partitions);
+
+#endif
--- Initial Partitioning/utils.hpp
+++ Initial Partitioning/utils.hpp
@@ -1,3 +1,6 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
@@ -26,11 +29,25 @@ struct FrontierNode {
     }
 };
 
+struct PartitionUpdate {
+    int partition_id;
+    int node;
+
+    PartitionUpdate() : partition_id(-1), node(-1) {}
+    PartitionUpdate(int pid, int num) : partition_id(pid), node(num) {}
+};
+
 void sync_vector(int procId, int sourceProc, std::vector<int> &vec);
-void sync_unordered_map(int proc, int sourceProc, std::unordered_map<int, int> &map);
 std::vector<int> serialize_node_info(const std::vector<NodeInfo> &nodes);
 std::vector<NodeInfo> deserialize_node_info(const std::vector<int> &buffer);
+std::vector<int> serialize_updates(const std::vector<PartitionUpdate> &updates);
+std::vector<PartitionUpdate> deserialize_updates(const std::vector<int> &buffer);
+void sync_updates(int procId, int nprocs, const std::vector<PartitionUpdate> &local_updates, std::vector<std::vector<int>> &partitions, std::unordered_set<int> &global_partitioned);
 std::vector<int> serialize_partitions(const std::vector<std::vector<int>> &partitions);
-std::vector<std::vector<int>> deserialize_partitons(const std::vector<int> &buffer);
-void sync_global_partitions(int procId, int nprocs, std::vector<std::vector<int>> &partitions, std::unordered_set<int> &global_partitioned);
+std::vector<std::vector<int>> deserialize_partitions(const std::vector<int> &buffer);
+void sync_global_partitions(int procId, int nprocs, std::vector<std::vector<int>> &partitions, std::unordered_set<int> &global_partitioned, std::vector<PartitionUpdate> &pending_updates);
+void add_node_to_partition(int node, int partition_id, std::vector<std::vector<int>> &partitions, std::unordered_set<int> &global_partitioned, std::vector<PartitionUpdate> &pending_updates);
 void seed_redistribution(int procId, int nprocs, int numParts, const std::vector<int> &remaining_seeds, const std::unordered_map<int, std::vector<int>> &local_adj, std::vector<std::vector<int>> &partitions, std::unordered_set<int> &global_partitioned);
+void print_summary(int procId, int nprocs, const std::vector<std::vector<int>> &partitions, const std::unordered_map<int, int> &global_degree);
+
+#endif
