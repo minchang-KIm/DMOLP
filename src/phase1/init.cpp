6d41b992219bd32a03b935b5eea195bd70e521b8
qkrdudwls <jacob717@ajou.ac.kr>
2025-08-12 15:03:49 +0900

    최적화: find_hub_nodes 및 find_first_seed 함수에서 불필요한 계산 제거 및 성능 개선

--- src/phase1/init.cpp
+++ src/phase1/init.cpp
@@ -142,30 +142,32 @@ vector<int> find_hub_nodes(const unordered_map<int, int> &global_degree) {
     });
 
     int N = sorted_degree.size();
-    vector<double> cum_x(N), cum_y(N);
+    vector<int> hub_nodes;
+    if (N < 2) return hub_nodes;
 
-    double total_deg = 0.0;
-    for (const auto &[_, deg] : sorted_degree)
-        total_deg += deg;
-
-    double sum_deg = 0.0;
+    vector<double> cum_y(N);
+    double total_deg = 0.0, sum_deg = 0.0;
     for (int i = 0; i < N; ++i) {
-        sum_deg += sorted_degree[i].second;
-        cum_x[i] = (double)(i + 1) / N;
-        cum_y[i] = sum_deg / total_deg;
+        int deg = sorted_degree[i].second;
+        total_deg += deg;
+        sum_deg += deg;
+        cum_y[i] = sum_deg;
     }
 
-    double x1 = cum_x[N - 1];
+    for (int i = 0; i < N; ++i)
+        cum_y[i] /= total_deg;
+
+    double inv_N = 1.0 / N;
+    double x1 = N * inv_N;
     double y1 = cum_y[N - 1];
-    double x0 = cum_x[N - 2];
+    double x0 = (N - 1) * inv_N;
     double y0 = cum_y[N - 2];
     double slope = (y1 - y0) / (x1 - x0);
-
     double x_intercept = x1 - y1 / slope;
 
-    vector<int> hub_nodes;
     for (int i = N - 1; i >= 0; --i) {
-        if (cum_x[i] > x_intercept) hub_nodes.push_back(sorted_degree[i].first);
+        double x = (i + 1) * inv_N;
+        if (x > x_intercept) hub_nodes.push_back(sorted_degree[i].first);
         else break;
     }
 
@@ -173,13 +175,11 @@ vector<int> find_hub_nodes(const unordered_map<int, int> &global_degree) {
 }
 
 pair<int, int> find_first_seed(const unordered_map<int, int> &global_degree) {
-    vector<pair<int, int>> sorted_degree(global_degree.begin(), global_degree.end());
-    sort(sorted_degree.begin(), sorted_degree.end(), [](const auto &a, const auto &b) {
-        return (a.second > b.second) || (a.second == b.second && a.first < b.first);
-    });
+    pair<int, int> first_seed = {-1, -1};
 
-    pair<int, int> first_seed;
-    if (!global_degree.empty()) first_seed = sorted_degree[0];
-    
+    for (const auto &[node, deg] : global_degree) {
+        if (deg > first_seed.second || (deg == first_seed.second && node < first_seed.second)) first_seed = {node, deg};
+    }
+
     return first_seed;
 }
